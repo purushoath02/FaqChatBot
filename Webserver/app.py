f8cde2b12e7da9f68db7eb6af1063ac287f7283b
purushoath02 <purushoath2002@protonmail.com>
2023-05-25 08:17:28 +0530

    Basic frontend

--- Webserver/app.py
+++ Webserver/app.py
@@ -1,58 +1,51 @@
-# from flask import Flask, render_template, request, jsonify
-# from rasa.core.agent import Agent
-
-# app = Flask(__name__)
-
-# # Load the trained Rasa model
-# model_path = "/home/edr/Kryos/FaqChatbot/ChatBot/models/20230522-022209-cold-hook.tar.gz"
-# agent = Agent.load(model_path)
-
-# # async def async_function():
-# #     # Your asynchronous code here
-# #     await asyncio.sleep(1)
-# #     return "Hello from async function!"
-
-# @app.route("/")
-# def index():
-    
-#     return render_template("index.html")
-
-# @app.route("/chat", methods=["POST"])
-# async def chat():
-#     # Get user message from the request
-#     user_message = request.json["message"]
-
-#     # Process user message using the Rasa agent
-#     response = agent.handle_text(user_message)
-#     #result = await async_function()
-#     # Extract the chatbot's response
-#     bot_response = response[0]["text"]
-#     #print(bot_response)
-#     # Return the response to the user
-#     return jsonify({"response": bot_response})
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template,jsonify
 import requests
 import json
+import re
  
 app =Flask(__name__, template_folder= 'templates')
 context_set = ""
- 
-@app.route('/', methods = ['POST', 'GET'])
+
+#Funtion to comuniacate with bot 
+def Botsend(s): 
+    #val = str(request.args.get('text'))
+    data =json.dumps({"sender": "Rasa","message": s})
+    headers ={'Content-type': 'application/json', 'Accept': 'text/plain'}
+    res =requests.post('http://localhost:5005/webhooks/rest/webhook', data= data, headers=headers)
+    res =res.json()
+    val = res[0]['text']
+    return val
+
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        val = str(request.args.get('text'))
-        data =json.dumps({"sender": "Rasa","message": val})
-        headers ={'Content-type': 'application/json', 'Accept': 'text/plain'}
-        res =requests.post('http://localhost:5005/webhooks/rest/webhook', data= data, headers=headers)
-        res =res.json()
-        val = res[0]['text']
-        return render_template('tempindex.html', val=val)
+    return render_template('tempindex.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    input_text = request.json['input']
+
+    # Perform processing with ChatGPT model
+    # Replace the following line with your ChatGPT code
+    response = Botsend(input_text)
+
     
+
+    # Check if the response is an image URL based on a regular expression pattern
+    image_pattern = r'(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)'
+    if re.match(image_pattern, response):
+        response_type = 'image'
+        print("Response is image")
+    else:
+        response_type = 'text'
+        print("Response is text")
+
+    #return jsonify(type=response_type, response=response+"::"+response_type)
+    return jsonify(type=response_type, response=response)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
 if __name__ == '__main__':
   app.run(debug=True)
